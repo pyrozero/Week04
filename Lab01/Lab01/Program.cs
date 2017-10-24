cfc8a8607ee69aa4b8db99472fa508008e8947aa
Falish <pyro.zero@hotmail.com>
2017-10-24 18:45:00 +0700

    Com:14

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,11 +1,27 @@
 ﻿using System;
-
-namespace Lab01
+namespace RandomNumber
 {
     class Program
     {
         static void Main(string[] args)
         {
+            ;
+            
+            Console.Write("Enter number : ");
+            int A = Console.Read();
+
+            Random random = new Random();
+            int randomNumber = random.Next(0,9);
+            Console.Write("Random Number is  : ");
+            Console.WriteLine(randomNumber);
+
+
+            Console.Write("Answer  : ");
+            bool B = A < randomNumber ;
+            Console.WriteLine("{0}", B);
+           
+
+
         }
     }
 }
