97614cb862d64e108b74ac27856d169fafa8fb98
Falish <pyro.zero@hotmail.com>
2017-10-24 15:14:43 +0700

    Com:Lab03P02

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
-
 namespace Lab01
 {
     class Program
     {
         static void Main(string[] args)
         {
+            char ch;
+            int  gg
+            Console.Write("Press a key followed by ENTER:");
+            ch = Console.Read();  // get a char
+           
+            Console.WriteLine("Your key is: " + ch);
+            
         }
     }
 }
