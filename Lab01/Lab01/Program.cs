6d87850c6931d6500d27c1db9aecbc46f6571731
Falish <pyro.zero@hotmail.com>
2017-10-24 14:32:35 +0700

    Com: 04

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -5,9 +5,9 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
-            Console.WriteLine("World.");
-
+            Console.WriteLine("This is text 1.");
+            Console.WriteLine("This is text 2.");
+            Console.WriteLine("This is text 3.");
         }
     }
 }
