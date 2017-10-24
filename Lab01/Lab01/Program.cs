e75f5b6faa3589f27b53011fd03cb51785420968
Falish <pyro.zero@hotmail.com>
2017-10-24 19:03:44 +0700

    Com:Lab03p05

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
-
-namespace Lab01
+public class intergerTest
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-        }
+        int a, b, c, d, e, f;
+        a = 1;
+        b = a + 6;
+        c = b - 3;
+        d = c * 2;
+        e = d / 2;
+        f = e % 2;
+
     }
 }
