2e329be0c21158028ea17ccf8e00cf9f6a1c5883
Falish <pyro.zero@hotmail.com>
2017-10-24 17:47:43 +0700

    Com:Lab03p03

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
-
-namespace Lab01
+namespace variableProperties
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Data type : int");
+            Console.WriteLine("Size :" + sizeof(int));
+            Console.WriteLine("Minimum Value :" + int.MinValue);
+            Console.WriteLine("Maximum Value :" + int.MaxValue);
         }
     }
 }
