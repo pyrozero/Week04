1f21d3cbbf77fe24a26f2576ee557a6262f618d1
Falish <pyro.zero@hotmail.com>
2017-10-24 17:44:43 +0700

    Com:Ex02

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -5,31 +5,16 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+
+            Console.Write("Name:");
+            string na = Console.ReadLine();
+            Console.Write("Lastname:");
+            string ln = Console.ReadLine();
+            Console.Write("ID :");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Console.Write("GPA:");
+            double gpa = Convert.ToDouble(Console.ReadLine());
             
-            string na, ln,ids;
-            double gpa;
-            Console.WriteLine("\t::Input::");
-            Console.Write("Please enter Name :");
-            na = Console.ReadLine();
-            Console.Write("Please enter Lastname :");
-            ln = Console.ReadLine();
-            Console.Write("Your ID:");
-            ids = Console.ReadLine();
-            Console.Write("GPA :");
-            gpa = Console.ReadLine();
-
-           
-            Console.WriteLine("\t::Output:: ");
-            Console.WriteLine("Name     :{0}",na);
-            Console.WriteLine("Lastname :{0}",ln);
-            Console.WriteLine("ID       :{0}",ids);
-
-            Console.WriteLine("GPA      :{0:F2}",gpa);
-
-
-
-
-
         }
     }
 }
