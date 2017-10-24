addd4d864281f0cbad31a3e562b618fe2fa7127c
Falish <pyro.zero@hotmail.com>
2017-10-24 14:24:13 +0700

    Com: Console.WriteLine

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -5,8 +5,7 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello");
-            Console.Write("Hello");
+            Console.WriteLine("Hello");
         }
     }
 }
