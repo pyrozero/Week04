92bdf864151dfd7f6dc48096401a945a3d4f8cea
Falish <pyro.zero@hotmail.com>
2017-10-24 19:29:22 +0700

    Com:17

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -3,13 +3,20 @@ public class intergerTest
 {
     static void Main(string[] args)
     {
-        int a, b, c, d, e, f;
-        a = 1;
-        b = a + 6;
-        c = b - 3;
-        d = c * 2;
-        e = d / 2;
-        f = e % 2;
-        Console.WriteLine(" a={0},b={1},c={2},d={3},e={4},f={5}",a,b,c,d,e,f);
+        int a=10, b=20, x=5, y=2 ;
+        int g1, g2, g3, g4, g5, g6, g7, g8, g9, g10;
+        g1=a + b;
+        g2= x - b;
+        g3= x * b;
+        g4= y / a;
+        g5= b % y;
+        g6= y + 10 % x;
+        g7= a / 3 * 5;
+        g8= 9 / 2 * a;
+        g9= y % 8;
+        g10= 100 * x + y % 2 - a;
+
+        Console.WriteLine(" 1={0}, 2={1}, 3={2}, 4={3}, 5={4}", g1, g2, g3, g4, g5 );
+        Console.WriteLine(" 6={0}, 7={1}, 8={2}, 9={3}, 10={4}", g6, g7, g8, g9, g10);
     }
 }
