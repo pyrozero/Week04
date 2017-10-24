e500435546f9423e63aab39fa1abb3e17bf59360
Falish <pyro.zero@hotmail.com>
2017-10-24 20:07:36 +0700

    Com:ex3

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
-
-namespace Lab01
+public class MathTest
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        for (float i = -2; i < Math.PI * 4.F; i += 0.35F)
         {
+            Console.WriteLine("The Cos of {0,10:F} = {1,-10:F6}" +
+            spaces(Math.Tan(i)) + "*", i, Math.Tan(i));
         }
+    }
+    private static string spaces(double val)
+    {
+        string SpaceString = new String(' ', ((int)(val * 1.0)) + 10);
+        return SpaceString;
     }
 }
